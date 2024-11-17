1c58192b6ae23b7070df4d4f8c2ea8e62ef807aa
Grupo5CicloWeb <cicloc504@gmail.com>
2024-11-17 10:20:37 -0500

    Algoritmica REST

--- src/main/java/pe/edu/cibertec/appfundamentosrest/controller/FundamentosController.java
+++ src/main/java/pe/edu/cibertec/appfundamentosrest/controller/FundamentosController.java
@@ -1,19 +1,57 @@
 package pe.edu.cibertec.appfundamentosrest.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.appfundamentosrest.service.FundamentosService;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/fundamentos")
 public class FundamentosController {
+
+    private final FundamentosService fundamentosService;
 
     @GetMapping("/ejercicio1")
     public ResponseEntity<String> Ejercicio1(){
         return new ResponseEntity<>("Hola Mundo Rest", HttpStatus.OK);
     }
+    @GetMapping("/primo")
+    public ResponseEntity<String> validarNumeroPrimo(
+            @RequestParam int numero){
+        return new ResponseEntity<>(
+                fundamentosService.validarNumeroPrimo(numero),
+                HttpStatus.OK);
+    }
 
+
+    @GetMapping("/factorial")
+    public ResponseEntity<String> calcularFactorial(
+            @RequestParam int numero){
+        return new ResponseEntity<>(
+                fundamentosService.calcularFactorial(numero),
+                HttpStatus.OK);
+    }
+    //localhost:8080/api/v1/fundamentos/fibonacci/5
+    @GetMapping("/fibonacci/{numero}")
+    public ResponseEntity<List<Integer>> calcularFibonacci(
+            @PathVariable int numero){
+        return new ResponseEntity<>(
+                fundamentosService.calcularFibonacci(
+                        numero),
+                HttpStatus.OK);
+    }
+    //localhost:8080/api/v1/fundamentos/suma/5?numero2=10
+    @GetMapping("/suma/{numero1}")
+    public ResponseEntity<String> suma(
+            @PathVariable int numero1,
+            @RequestParam int numero2){
+        return new ResponseEntity<>(
+                String.valueOf(numero1 + numero2),
+                HttpStatus.OK);
+    }
 
 }
